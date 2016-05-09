82e80b0ab01b9ee7ab730987b91649ed2e29f959
Sammako <marie.aurelle@free.fr>
2016-05-09 14:11:00 +0200

    maj

---
+++ exo3TD1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[]){
+	
+	FILE *fp = open("toto.txt",O_RDONLY);
+	
+	
+	return 0;
+}
